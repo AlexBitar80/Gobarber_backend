af2fbcad51de4312bcc0d15e03ce378719e329e4
Alexandre <joabitar76@gmail.com>
2020-10-26 19:58:17 -0300

    chore: added new field in table id that defaults to uuid

--- src/database/migrations/1603745788968-create-appointments.ts
+++ src/database/migrations/1603745788968-create-appointments.ts
@@ -12,6 +12,7 @@ export default class createAppointments1603745788968
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
