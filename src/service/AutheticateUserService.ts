6a7eb3938de2fd1fbdcc4e74b80bab650f1cd023
Alexandre <joabitar76@gmail.com>
2020-11-07 12:53:35 -0300

    refactor: changing the secret and expiration time to another file

--- src/service/AutheticateUserService.ts
+++ src/service/AutheticateUserService.ts
@@ -1,7 +1,8 @@
 import { compare } from 'bcryptjs';
 import { getRepository } from 'typeorm';
-
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
+
 import User from '../models/User';
 
 interface Request {
@@ -30,9 +31,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, '52004762a77f2cd73495ebd8ca68c8fe', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
