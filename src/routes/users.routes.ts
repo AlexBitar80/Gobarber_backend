1150c73fd6c90ee5116f1cddca314d4fd82cffe9
Alexandre <joabitar76@gmail.com>
2020-11-11 19:59:35 -0300

    chore: 📤 added route that adds and updates user avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 import CreateUserService from '../service/CreateUserService';
+import UpdateUserAvatarService from '../service/UpdateUserAvatarService';
 
 const UsersRoute = Router();
+const upload = multer(uploadConfig);
 
 UsersRoute.post('/', async (request, response) => {
   try {
@@ -30,4 +38,32 @@ UsersRoute.post('/', async (request, response) => {
   }
 });
 
+UsersRoute.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      const userWithoutPassword = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      };
+
+      return response.json(userWithoutPassword);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
 export default UsersRoute;
