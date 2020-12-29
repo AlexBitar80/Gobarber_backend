f0bda4e7af575290de4523f7f3a52d9a8b97f631
Alexandre Bitar <joabitar76@gmail.com>
2020-12-29 14:54:09 -0300

    feat: moved logic of user routes to the controller

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -4,60 +4,22 @@ import multer from 'multer';
 
 import uploadConfig from '@config/upload';
 
-import CreateUserService from '@modules/users/services/CreateUserService';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
+
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const UsersRoute = Router();
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 const upload = multer(uploadConfig);
 
-UsersRoute.post('/', async (request, response) => {
-  const { name, email, password } = request.body;
-
-  const usersRepository = new UsersRepository();
-  const createUser = new CreateUserService(usersRepository);
-
-  const user = await createUser.execute({
-    name,
-    email,
-    password,
-  });
-
-  const userWithoutPassword = {
-    id: user.id,
-    name: user.name,
-    email: user.email,
-    created_at: user.created_at,
-    updated_at: user.updated_at,
-  };
-
-  return response.json(userWithoutPassword);
-});
+UsersRoute.post('/', usersController.create);
 
 UsersRoute.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  async (request, response) => {
-    const usersRepository = new UsersRepository();
-    const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
-
-    const user = await updateUserAvatar.execute({
-      user_id: request.user.id,
-      avatarFilename: request.file.filename,
-    });
-
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      avatar: user.avatar,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
-
-    return response.json({ user: userWithoutPassword });
-  },
+  userAvatarController.update,
 );
 export default UsersRoute;
