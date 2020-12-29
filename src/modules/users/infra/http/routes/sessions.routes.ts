1a4a3b7de185e7ac1e4ce952b74c948cc9004670
Alexandre Bitar <joabitar76@gmail.com>
2020-12-29 14:54:37 -0300

    feat: moved session route logic to the controller

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,30 +1,10 @@
 import { Router } from 'express';
 
-import AuthenticateUserService from '@modules/users/services/AutheticateUserService';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import SessionsController from '../controllers/SessionsController';
 
 const sessionsRoute = Router();
+const sessionsControlle = new SessionsController();
 
-sessionsRoute.post('/', async (request, response) => {
-  const { email, password } = request.body;
-
-  const usersRepository = new UsersRepository();
-  const authenticateUser = new AuthenticateUserService(usersRepository);
-
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password,
-  });
-
-  const userWithoutPassword = {
-    id: user.id,
-    name: user.name,
-    email: user.email,
-    created_at: user.created_at,
-    updated_at: user.updated_at,
-  };
-
-  return response.json({ user: userWithoutPassword, token });
-});
+sessionsRoute.post('/', sessionsControlle.create);
 
 export default sessionsRoute;
