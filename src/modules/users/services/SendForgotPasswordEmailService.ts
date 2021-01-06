4c11cb79028ec63e7cfe87e1f12d6dd9ad2575a9
Alexandre Bitar <joabitar76@gmail.com>
2021-01-06 00:27:29 -0300

    feat: added class to handle password recovery requests

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,42 @@
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import AppError from '@shared/errors/AppError';
+import { inject, injectable } from 'tsyringe';
+
+// import User from '@modules/users/infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private usersTokensRepository: IUserTokensRepository,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+
+    const { token } = await this.usersTokensRepository.generate(user.id);
+
+    await this.mailProvider.sendMail(
+      email,
+      `Pedido de recuperação de senha recebido: ${token}`,
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
