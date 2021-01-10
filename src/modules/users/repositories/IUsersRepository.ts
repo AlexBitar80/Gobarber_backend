f9c057e99e05fc45e08e4b191ac55254af3f72c3
Alexandre Bitar <joabitar76@gmail.com>
2021-01-09 23:32:22 -0300

    feat: added method to find all providers

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,9 +1,11 @@
 import ICreateUserDTO from '../dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '../dtos/IFindAllProviderDTO';
 import User from '../infra/typeorm/entities/User';
 
 export default interface IUsersRepository {
   findById(id: string): Promise<User | undefined>;
   findByEmail(emai: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
+  findAllProviders(data: IFindAllProvidersDTO): Promise<User[]>;
   save(user: User): Promise<User>;
 }
