2da5eb99ce99a3787bfc30516ca30247cdd4846e
Alexandre Bitar <joabitar76@gmail.com>
2021-01-12 14:12:06 -0300

    chore: added route validation

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,3 +1,4 @@
+import { celebrate, Joi, Segments } from 'celebrate';
 import { Router } from 'express';
 
 import ProfileController from '../controllers/ProfileController';
@@ -9,6 +10,25 @@ const profileController = new ProfileController();
 profileRouter.use(ensureAuthenticated);
 
 profileRouter.get('/', profileController.show);
-profileRouter.put('/', profileController.update);
+
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.when('old_password', {
+        is: Joi.exist(),
+        then: Joi.required(),
+      }),
+      password_confirmation: Joi.when('password', {
+        is: Joi.exist(),
+        then: Joi.valid(Joi.ref('password')).required(),
+      }),
+    },
+  }),
+  profileController.update,
+);
 
 export default profileRouter;
--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,3 +1,4 @@
+import { celebrate, Joi, Segments } from 'celebrate';
 import { Router } from 'express';
 
 import SessionsController from '../controllers/SessionsController';
@@ -5,6 +6,15 @@ import SessionsController from '../controllers/SessionsController';
 const sessionsRoute = Router();
 const sessionsControlle = new SessionsController();
 
-sessionsRoute.post('/', sessionsControlle.create);
+sessionsRoute.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  sessionsControlle.create,
+);
 
 export default sessionsRoute;
