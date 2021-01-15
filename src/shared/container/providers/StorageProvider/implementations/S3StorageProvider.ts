4f9ffeeaebf1016e611a0786b3d8abacc40b42c3
Alexandre Bitar <joabitar76@gmail.com>
2021-01-14 22:48:08 -0300

    chore: added class responsible for the logic of the methods of sending files to Amazon S3

---
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import path from 'path';
+import mime from 'mime';
+import uploadConfig from '@config/upload';
+import AWS, { S3 } from 'aws-sdk';
+
+import IStorageProvider from '../models/IStorageProvider';
+
+export default class S3StorageProvider implements IStorageProvider {
+  private client: S3;
+
+  constructor() {
+    this.client = new AWS.S3({
+      region: '',
+    });
+  }
+
+  public async saveFile(file: string): Promise<string> {
+    const originalPath = path.resolve(uploadConfig.tmpFolder, file);
+
+    const ContentType = mime.getType(originalPath);
+
+    const fileContent = await fs.promises.readFile(originalPath);
+
+    if (!ContentType) {
+      throw new Error('File not found');
+    }
+
+    await this.client
+      .putObject({
+        Bucket: uploadConfig.config.aws.bucket,
+        Key: file,
+        ACL: 'public-read',
+        Body: fileContent,
+        ContentType,
+      })
+      .promise();
+
+    await fs.promises.unlink(originalPath);
+
+    return file;
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    await this.client
+      .deleteObject({
+        Bucket: uploadConfig.config.aws.bucket,
+        Key: file,
+      })
+      .promise();
+  }
+}
